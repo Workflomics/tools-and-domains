8c1c7d5000da63c3978c770b6a5ef3b1d9bd0509
Peter Kok <kretep@users.noreply.github.com>
2024-10-03 13:15:21 +0200

    Simplify

--- cwl-tools/Sage/test/run-cwl.sh
+++ cwl-tools/Sage/test/run-cwl.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 cwltool --outdir output ../sage.cwl ./input.yml
-
-# cwltool --enable-ext --docker --debug \
-#   --overrides '{"requirements": {"DockerRequirement": {"dockerVolumes": ["/var/run/docker.sock:/var/run/docker.sock"]}}}' \
-#   --output output \
-#   ../sage.cwl ./input.yml
